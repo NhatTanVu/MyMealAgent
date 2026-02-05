101a889162785a8a3ae8d200e5c816ffc9586bf6
Nhat Tan Vu <onchua2006@gmail.com>
2026-02-04 22:08:36 -0800

    Fix rendering issue on Import screen

--- frontend/mealagent-mobile/app/import/import-notice.tsx
+++ frontend/mealagent-mobile/app/import/import-notice.tsx
@@ -9,7 +9,7 @@ export default function ImportNotice({ focused }: Props) {
     return (
         <>
             <ThemedText style={styles.container}>
-                ⚠️ <b>TikTok & Dailymotion</b> URLs are supported.<br/> <b>YouTube and Instagram</b> URLs are not supported due to platform restrictions.
+                ⚠️ <ThemedText style={styles.bold_text}>TikTok & Dailymotion</ThemedText> URLs are supported. <ThemedText style={styles.bold_text}>YouTube and Instagram</ThemedText> URLs are not supported due to platform restrictions.
             </ThemedText>
             {/* {focused ? (
                 <ThemedText style={styles.container}>
@@ -35,10 +35,17 @@ export default function ImportNotice({ focused }: Props) {
 
 const styles = StyleSheet.create({
     container: {
-        padding: 12,
+        padding: 8,
         borderRadius: 8,
         backgroundColor: "#FFF4E5",
-        marginBottom: 12
+        marginBottom: 8,
+        fontSize: 12,
+        color: "#B45309"
+    },
+    bold_text: {
+        fontWeight: "bold",
+        fontSize: 14,
+        color: "#B45309"
     },
     title: {
         fontWeight: "bold",
